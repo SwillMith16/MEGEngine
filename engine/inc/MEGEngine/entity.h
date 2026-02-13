576aad82877597663a800f592a73d464a98a1eb1
Will <ws.will09@gmail.com>
2026-02-13 21:41:54 +0000

    Removed redundant input code

--- engine/inc/MEGEngine/entity.h
+++ engine/inc/MEGEngine/entity.h
@@ -25,25 +25,11 @@ namespace MEGEngine {
 		[[nodiscard]] const std::vector<Entity*>& children() const;
 		[[nodiscard]] Entity* parent() const;
 
-		// Template allows for sub-types of EventListener to be attached to an entity
-		template<typename ListenerType, typename EventType>
-		void createEventListener()
-		{
-			static_assert(std::is_base_of_v<EventListener, ListenerType>);
-			static_assert(std::is_base_of_v<Event, EventType>);
-
-			auto listener = std::make_unique<ListenerType>(*this);
-			ListenerType& ref = *listener;
-			_listeners.push_back(std::move(listener));
-			EventManager::addListener<EventType>(ref);
-		}
-
 	protected:
 		Entity* _parent = nullptr;
 		std::vector<Entity*> _children;
 		std::unique_ptr<Transform> _transform = std::make_unique<Transform>();
 		std::shared_ptr<MeshRenderer> _meshRenderer;
-		std::vector<std::unique_ptr<EventListener>> _listeners;
 	};
 }
 
